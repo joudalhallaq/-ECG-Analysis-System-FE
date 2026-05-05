35c57f367c1ef7c722facb524560d58f9d723a0a
joudalhallaq <156162712+joudalhallaq@users.noreply.github.com>
2026-05-05 12:40:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import Login from './Login'
-import Register from './Register'
-import Dashboard from './Dashboard'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./login.jsx";
+import Register from "./Register.jsx";
+import Dashboard from "./Dashboard.jsx";
+import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
